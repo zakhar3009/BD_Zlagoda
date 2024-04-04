6def066ab40ffd62c4ee42ce92cbb0904241f136
Striletska Kateryna <striletskay2005@gmail.com>
2024-04-04 23:48:37 +0300

    Add new Shop page

---
+++ src/main/webapp/react-app/src/components/mainPage/Info.jsx
@@ -0,0 +1,10 @@
+
+export default function Info() {
+
+    return (
+    <div>
+        <label> There will be result of request!</label>
+
+    </div>
+    );
+}
--- src/main/webapp/react-app/src/pages/Shop/Shop.jsx
+++ src/main/webapp/react-app/src/pages/Shop/Shop.jsx
@@ -1,8 +1,51 @@
 import React from 'react'
-
+import {useState} from "react";
+import "./Style.css";
+import Info from "../../components/mainPage/Info.jsx";
 const Shop = () => {
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const fetchData = async (command) => {
+        try {
+            setIsLoading(true);
+            const response = await fetch(
+                "http://localhost:8080/controller?" +
+                new URLSearchParams({
+                    "command_name": command,
+                })
+            );
+            const data = await response.json();
+            setData(data);
+            setIsLoading(false);
+            console.log(data);
+        } catch (err) {
+            console.log(err);
+        }
+    };
     return (
-        <div>Shop Component</div>
+<div className={"shop"}>
+
+
+<div class="w-48 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+    <a href="#"
+       onClick={()=> fetchData("GET_ALL_EMPLOYEES")}
+       aria-current="true"
+       class="block w-full px-4 py-2 text-white bg-blue-700 border-b border-gray-200 rounded-t-lg cursor-pointer dark:bg-gray-800 dark:border-gray-600">
+        Get all employees order by surname
+    </a>
+    <a href="#" class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+        Get employee by ID
+    </a>
+    <a href="#" class="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+        Create new employee
+    </a>
+    <a href="#" class="block w-full px-4 py-2 rounded-b-lg cursor-pointer hover:bg-gray-100 hover:text-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white">
+        Edit employee
+    </a>
+</div>
+<Info></Info>
+    </div>
     )
 }
 
